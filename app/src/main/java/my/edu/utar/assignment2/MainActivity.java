10a5e449a833e6f01cdb1547b865ac1670c621c5
Lilia-Kim <steffiyim7532@gmail.com>
2025-04-14 14:40:26 +0800

    steffi_testcommit2

--- app/src/main/java/my/edu/utar/assignment2/MainActivity.java
+++ app/src/main/java/my/edu/utar/assignment2/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // zilin test commit
         //steffi test commit
+        //steffi test commit 2
     }
 }
