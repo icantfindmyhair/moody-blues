bab4673af71d13664ff03c1b80f6deb9e7fbe481
Zi Lin! <zilinzilinzilin02@gmail.com>
2025-04-22 02:45:02 +0800

    home page initial set up

--- app/src/main/java/my/edu/utar/assignment2/MainActivity.java
+++ app/src/main/java/my/edu/utar/assignment2/MainActivity.java
@@ -1,15 +1,26 @@
 package my.edu.utar.assignment2;
 
+import android.os.Bundle;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView dateTimeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // zilin test commit
+
+        dateTimeText = findViewById(R.id.dateTimeText);
+
+        // Set current date and time
+        String currentDateTime = new SimpleDateFormat("EEEE, MMM d yyyy\nhh:mm a", Locale.getDefault()).format(new Date());
+        dateTimeText.setText(currentDateTime);
     }
 }
