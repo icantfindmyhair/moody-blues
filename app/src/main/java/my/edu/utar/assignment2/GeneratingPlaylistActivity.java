cf3243d9343522d6fd6b808c60035d4cae43cc78
Zi Lin! <zilinzilinzilin02@gmail.com>
2025-04-24 01:49:13 +0800

    record mood data in local storage + generate playlist activity

---
+++ app/src/main/java/my/edu/utar/assignment2/GeneratingPlaylistActivity.java
@@ -0,0 +1,14 @@
+package my.edu.utar.assignment2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GeneratingPlaylistActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generating_playlist);
+    }
+}
--- app/src/main/java/my/edu/utar/assignment2/MoodLogging.java
+++ app/src/main/java/my/edu/utar/assignment2/MoodLogging.java
@@ -1,21 +1,30 @@
 package my.edu.utar.assignment2;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MoodLogging extends AppCompatActivity {
+
     private ImageView moodIcon;
     private EditText moodInput;
     private TextView moodText;
+    private Button doneButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +34,10 @@ public class MoodLogging extends AppCompatActivity {
         moodIcon = findViewById(R.id.moodIcon);
         moodInput = findViewById(R.id.moodInput);
         moodText = findViewById(R.id.moodText);
+        doneButton = findViewById(R.id.doneButton);
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MoodLoggingPrefs", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
 
         Intent intent = getIntent();
         int iconResId = intent.getIntExtra("mood_icon", R.drawable.excited_icon);
@@ -41,5 +54,37 @@ public class MoodLogging extends AppCompatActivity {
             }
         });
 
+        // -------------- after click done, store data and show alert box ----------------
+        // no to return home, yes to generate playlist
+        doneButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentDate = java.text.DateFormat.getDateTimeInstance().format(new java.util.Date());
+
+                String mood = moodInput.getText().toString();
+
+                editor.putString("date", currentDate);
+                editor.putString("time", currentDate);
+                editor.putString("weather", "");  // empty string for now cuz weather api not done
+                editor.putString("mood", mood);
+                editor.putString("moodInput", mood);
+
+                editor.apply();
+
+                YesNoDialogBox.show(
+                        MoodLogging.this,
+                        "Mood recorded!",
+                        "Would you like to generate a playlist based on your mood?",
+                        "Yes", "No",
+                        yesView -> {
+                            Intent intent = new Intent(MoodLogging.this, GeneratingPlaylistActivity.class);
+                            startActivity(intent);
+                        },
+                        noView -> finish()
+                );
+
+
+            }
+        });
     }
 }
