6a0e85b85e161a74dd816c240773dc5e34a0695e
Zi Lin! <zilinzilinzilin02@gmail.com>
2025-05-01 22:02:20 +0800

    Revert "splash screen animation + app icon"
    This reverts commit 0c038626b007bfb43f7aeaaeaa8077ad0fbb9a48.

--- app/src/main/java/my/edu/utar/assignment2/SplashActivity.java
+++ app/src/main/java/my/edu/utar/assignment2/SplashActivity.java
@@ -1,13 +1,11 @@
 package my.edu.utar.assignment2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.animation.Animation;
-import android.view.animation.AnimationUtils;
-import android.view.animation.AnimationSet;
-import android.widget.ImageView;
+import android.view.WindowManager;
+
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
     @Override
@@ -15,22 +13,14 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        ImageView logo = findViewById(R.id.logoImageView);
-
-        Animation rotate = AnimationUtils.loadAnimation(this, R.anim.rotate);
-        Animation blinkZoom = AnimationUtils.loadAnimation(this, R.anim.blink);
-
-        AnimationSet animationSet = new AnimationSet(true);
-        animationSet.addAnimation(rotate);
-        animationSet.addAnimation(blinkZoom);
-
-        logo.startAnimation(animationSet);
+        getWindow().setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         new Handler().postDelayed(() -> {
             startActivity(new Intent(SplashActivity.this, MainActivity.class));
             finish();
-        }, 2000);
+        }, 2300);
     }
 }
-
 
